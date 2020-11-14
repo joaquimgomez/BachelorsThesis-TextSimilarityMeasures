ee206c55c28c668c6d951450abc1450490f1fdce
Joaquim Gomez <9joaquimgomez@gmail.com>
2020-11-14 18:52:34 +0100

    Clustering class

--- src/clustering/clustering.py
+++ src/clustering/clustering.py
@@ -7,26 +7,43 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-from similarities import similarities as sim
-from scipy.cluster.hierarchy import ward, fcluster,
+from scipy.cluster.hierarchy import linkage
 from scipy.cluster.hierarchy import dendrogram as dendro
+import src.similarities.similarities as sim
+import numpy as np
 
 class Clustering():
-    def distanceMatrix(self, embedding, measure, relaxed):
-        n, m = len(self.documents)
+    def computeDistanceMatrix(self, measure, relaxed):
+        print("Creating condensed distance matrix...")
+
+        n = len(self.documents)
+        m = n
         mat = np.zeros((n, m))
 
         for i in range(0, n):
-            for j in range(0, m):
+            for j in range(0, i + 1):
+                print("Computing (" + str(i) + ", " + str(j) + ") of " + "(" + str(n) + ", " + str(m) + ")...")
                 if (relaxed != -1):
-                    mat[i,j] = self.embedding.distance(self.documents[i], self.documents[j])
+                    mat[i,j] = self.embedding.distance(self.documents[i], self.documents[j], measure)
                 else:
-                    mat[i,j] = self.embedding.distance(self.documents[i], self.documents[j], relaxed)
+                    mat[i,j] = self.embedding.distance(self.documents[i], self.documents[j], measure, relaxed)
+
+        self.distMat = mat
+        print("Condensed distance matrix created.")
 
         return mat
 
+    def loadDistanceMatrix(self, path):
+        pass
+
+    def saveDistanceMateix(self, path):
+        mat = np.matrix(self.distMat)
+        with open(path, 'wb') as f:
+            for line in mat:
+                np.savetxt(f, line, fmt='%.2f')
+
     def hierarchicalClustering(self):
-        self.hierClust = ward(self.distMat)
+        self.hierClust = linkage(self.distMat, 'ward')
 
     def dendrogram(self):
         return dendro(self.hierClust)
@@ -41,15 +58,11 @@ class Clustering():
         elif (embedding == "d2v"):
             return sim.Doc2VecSimilarity(embeddingModelPath)
 
-    def __init__(self, documents, embedding, embeddingModelPath, measure, relaxed):
+    def __init__(self, documents, embedding, embeddingModelPath, embeddingSpacyModelPath):
         print("Creating Clustering object...")
         self.documents = documents
         print("Clustering object created.")
 
         print("Creating internal objects for the distance computation...")
-        self.embedding = createEmbeddingObject(embedding, embeddingModelPath, embeddingSpacyModelPath)
+        self.embedding = self.createEmbeddingObject(embedding, embeddingModelPath, embeddingSpacyModelPath)
         print("Internal objects for the distance computation created.")
-
-        print("Creating condensed distance matrix...")
-        self.distMat = distanceMatrix(embedding, measure, relaxed)
-        print("condensed distance matrix created")
